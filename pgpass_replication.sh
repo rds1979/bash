814234d1cd3448dc4f0a7d521a6bd6a1c3a4a150
Dmitriy Redkin <expert.info79@gmail.com>
2021-12-20 23:19:41 +0300

    Update pgpass_replication.sh

--- pgpass_replication.sh
+++ pgpass_replication.sh
@@ -34,9 +34,7 @@ EOF
 sudo -iu postgres chmod 600 /var/lib/pgsql/.pgpass
 sudo -iu postgres cp $PGDATA/pg_hba.conf $PGDATA/pg_hba.default
 sudo -iu postgres cp $PGDATA/pg_hba.conf $PGDATA/pg_hba.conf.`date +%Y%m%d`
-sudo -iu postgres chmod 666 $PGDATA/pg_hba.conf
-sudo -iu postgres echo "host    replication    replication_user    $REPLICA/32    md5" | sudo -iu postgres >> $PGDATA/pg_hba.conf
-sudo -iu postgres chmod 600 $PGDATA/pg_hba.conf
+sudo -iu postgres echo "host    replication    replication_user    $REPLICA/32    md5" | sudo -iu postgres tee -a $PGDATA/pg_hba.conf
 #sudo salt-call state.highstate
 #sudo -iu postgres psql -c "SELECT pg_reload_conf()"
 
